248cc0f27a44761d45b8cb2a7be54147d8eea4cb
Kapish <kapish.k.1@gmail.com>
2017-02-06 15:30:50 -0500

    added react stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
+import Message from './Message.jsx';
+import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <html>
+      <body>
+      <div class="wrapper">
+        <nav>
+          <h1>Chatty</h1>
+        </nav>
+        <div id="message-list">
+          <MessageList />
+          <Message />
+        </div>
+          <ChatBar />
+      </div>
+    </body>
+    </html>
     );
   }
 }
---
+++ src/MessageList.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class MessageList extends Component {
+  render() {
+    return (
+    <div class="message">
+      <span class="username">Anonymous1</span>
+      <span class="content"> I wont be impressed with technology until I can download food.</span>
+    </div>
+    );
+  }
+}
+export default MessageList;
